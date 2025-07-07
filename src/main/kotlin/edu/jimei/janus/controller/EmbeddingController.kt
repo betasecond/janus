ad5fc3b4cbdcbafcb2eff4850d2d38170475eb71
betasecond <nbdhc9039@gmail.com>
2025-07-07 13:45:41 +0800

    refactor(embedding): refactor EmbeddingController to use VOs

--- src/main/kotlin/edu/jimei/janus/controller/EmbeddingController.kt
+++ src/main/kotlin/edu/jimei/janus/controller/EmbeddingController.kt
@@ -1,14 +1,14 @@
 package edu.jimei.janus.controller
 
 import edu.jimei.janus.application.service.EmbeddingService
+import edu.jimei.janus.controller.vo.EmbeddingResponseVO
+import edu.jimei.janus.controller.vo.common.StatusVO
 import org.springframework.ai.embedding.EmbeddingModel
 import org.springframework.ai.embedding.EmbeddingResponse
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 
@@ -22,9 +22,9 @@ class EmbeddingController(
 ) {
 
     @PostMapping("/embedding/store")
-    fun embedAndStore(@RequestBody request: EmbeddingRequest): ResponseEntity<Map<String, String>> {
+    fun embedAndStore(@RequestBody request: EmbeddingRequest): ResponseEntity<StatusVO> {
         embeddingService.embedAndStore(request.message)
-        return ResponseEntity.ok(mapOf("status" to "success", "message" to "Embedding stored successfully."))
+        return ResponseEntity.ok(StatusVO(status = "success", message = "Embedding stored successfully."))
     }
 
 //    @GetMapping("/embedding/similar")
@@ -34,8 +34,8 @@ class EmbeddingController(
 //    }
 
     @PostMapping("/embedding")
-    fun embed(@RequestBody request: EmbeddingRequest): Map<String, EmbeddingResponse> {
+    fun embed(@RequestBody request: EmbeddingRequest): EmbeddingResponseVO {
         val embeddingResponse = embeddingModel.embedForResponse(listOf(request.message))
-        return mapOf("embedding" to embeddingResponse)
+        return EmbeddingResponseVO(embedding = embeddingResponse)
     }
 }
